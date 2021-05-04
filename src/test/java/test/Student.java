fac7fbdab894a48f9e66f712ebe3a612fa29199f
darevyua0 <qiquanliang66@126.com>
2021-05-04 20:30:04 +0800

    jdk8

---
+++ src/test/java/test/Student.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Created by crj on 2017/7/25.
+ */
+public class Student {
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
