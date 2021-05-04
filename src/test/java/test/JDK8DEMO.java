3562dbdc5a897eac69b1f02ffbee0c3e81786854
darevyua0 <qiquanliang66@126.com>
2021-05-04 20:42:16 +0800

    jdk8

--- src/test/java/test/JDK8DEMO.java
+++ src/test/java/test/JDK8DEMO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by crj on 2017/7/25.
@@ -14,21 +15,27 @@ public class JDK8DEMO {
        Student student1 = new Student();
        student1.setAge("12");
         Student student2 = new Student();
-        student1.setAge("13");
+        student2.setAge("13");
         Student student3 = new Student();
-        student1.setAge("11");
+        student3.setAge("11");
         Student student4 = new Student();
-        student1.setAge("18");
+        student4.setAge("18");
 
         list.add(student1);
         list.add(student2);
         list.add(student3);
         list.add(student4);
-        List<Demo> demos = list.stream().map(student-> new Demo(student.getAge(),40)).collect(Collectors.toList());
+        //传值
+        List<Demo> demos = list.stream().map(student-> new Demo(student.getAge(), 16)).collect(Collectors.toList());
         demos.forEach(demo -> {
             System.out.println(demo.getAge() +" " +demo.getSex());
         });
-       // demos.stream().filter(demo -> {demo.getAge() == null});
+        //筛选
+        List<Demo> demoFilter=  demos.stream().filter(demo -> Integer.valueOf(demo.getAge())>12).collect(Collectors.toList());
+        System.out.println("****************");
+       demoFilter.forEach(demo -> {
+           System.out.println(demo.getAge() +" " +demo.getSex());
+       });
 
     }
 }
